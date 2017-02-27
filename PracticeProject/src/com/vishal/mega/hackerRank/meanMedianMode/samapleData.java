24dc80da0bdc3386649626f3582c570790dd252f
sharm <sharmajitu2009@gmail.com>
2017-02-27 18:53:11 +0530

    Java -- sampleData -- #5,#6 -- 270220171853

--- PracticeProject/src/com/vishal/mega/hackerRank/meanMedianMode/samapleData.java
+++ PracticeProject/src/com/vishal/mega/hackerRank/meanMedianMode/samapleData.java
@@ -2,10 +2,10 @@ package com.vishal.mega.hackerRank.meanMedianMode;
 
 public class samapleData 
 {
-	//1// 10
+	//Sample Data 1// 10
 	   // 10 20 30 40 50 60 70 80 90 100
 	
-	//2//  13 
+	//Sample Data 2//  13 
 		// 10 10 20 20 20 30 30 40 50 60 70 80 90
 	
 	
