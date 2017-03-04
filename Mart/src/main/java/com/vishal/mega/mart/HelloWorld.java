f32df65ea5260699f2ce468fb3ce00fefd4ffeb7
sharm <sharmajitu2009@gmail.com>
2017-03-04 22:40:11 +0530

    Java -- Add Test Cases -- 01032017_2240

--- Mart/src/main/java/com/vishal/mega/mart/HelloWorld.java
+++ Mart/src/main/java/com/vishal/mega/mart/HelloWorld.java
@@ -1,9 +1,18 @@
 package com.vishal.mega.mart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloWorld 
 {
+	private static final Logger LOGGER=LoggerFactory.getLogger(HelloWorld.class);
+	
+	HelloWorld()
+	{
+		
+	}
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello Jeetu");
+		LOGGER.info("Hello Jeetu");
 	}
 }
