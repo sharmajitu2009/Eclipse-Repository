da4fab04327ef0062b2f3681c5eb47db8c13ad4f
sharm <sharmajitu2009@gmail.com>
2017-04-01 17:38:44 +0530

    Edit

--- Hello/src/main/java/com/my/app/Hello/MyResource.java
+++ Hello/src/main/java/com/my/app/Hello/MyResource.java
@@ -23,7 +23,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Jeetu Hardeniya!";
+        return "Jitendra Hardeniya!";
     }
     
     @POST
